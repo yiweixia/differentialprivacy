c29aead765d54aaa2fc240dd346ab85afaa06864
yiwei x <yiwei.x3@gmail.com>
2017-04-12 17:40:00 -0400

    better splits for data

--- cleaner.py
+++ cleaner.py
@@ -10,15 +10,57 @@ from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("raw.csv")
 
-for column in ['sales', 'salary']:
+df = df.rename(columns = {'sales': 'job'})
+
+for column in ['job', 'salary']:
     dummies = pd.get_dummies(df[column])
-    df[dummies.columns] = dummies
+    df[column + "_" + dummies.columns] = dummies
 
 del df['salary']
+del df['job']
 
 df.to_csv("processed.csv")
 
-def split(df):
+def split_train_test(df):
     train, test = train_test_split(df, test_size=.2, random_state=42)
-    train.to_csv("train_80.csv")
-    test.to_csv("test_20.csv")
+    return{"train":train, "test":test}
+
+# categorizes boolean 1/0 values into 1,2,3
+# e.g. categorize(df, "salary") will return a new dataframe with one category salary_categorized = {1,2,3}
+def categorize(df, starting_string):
+    i = 0
+    new_name = starting_string + "_categorized"
+    df[new_name] = 0;
+    for column in list(df):
+        if column.startswith(starting_string) and column != new_name:
+            df[column] = df[column]*i
+            df[new_name] = df[new_name] + df[column]
+            del df[column]
+            i += 1
+    
+# splits dataframe columns based on starting string
+# e.g. split_xy(df, "salary") will have salary_high/medium/low as Y, and everything else as X
+def split_xy(df, starting_string):
+    y = df.filter(regex='^' + starting_string, axis=1)
+    for column in list(df):
+        if column.startswith(starting_string):
+            del df[column]
+    x = df
+    return {"x":x, "y":y}
+
+def super_split(df, starting_string):
+    categorize(df, starting_string)
+    tt_split = split_train_test(df)
+    train = tt_split['train']
+    test = tt_split['test']
+    train_xy = split_xy(train, starting_string)
+    test_xy = split_xy(test, starting_string)
+    train_xy['x'].to_csv("data/salary_categorical/train_x.csv")
+    train_xy['y'].to_csv("data/salary_categorical/train_y.csv")
+    test_xy['x'].to_csv("data/salary_categorical/test_x.csv")
+    test_xy['y'].to_csv("data/salary_categorical/test_y.csv")
+    
+def super_split_salary():
+    super_split(df, "salary")
+        
+    
--- decision_trees.py
+++ decision_trees.py
@@ -6,4 +6,16 @@ Created on Wed Apr 12 15:21:29 2017
 """
 
 from sklearn import tree
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import linear_model, datasets
 
+
+
+# import some data to play with
+X = pd.read_csv("data/salary_categorical/train_x.csv")
+Y = pd.read_csv("data/salary_categorical/train_y.csv")
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X, Y)
