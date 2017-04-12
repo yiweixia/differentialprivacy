cbaabeabbceca84adc5142c7520723ac00de3b17
yiwei x <yiwei.x3@gmail.com>
2017-04-12 16:24:09 -0400

    split data

--- cleaner.py
+++ cleaner.py
@@ -5,8 +5,8 @@ Created on Wed Apr 12 15:02:23 2017
 @author: Yiwei Xia
 """
 
-import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("raw.csv")
 
@@ -17,3 +17,8 @@ for column in ['sales', 'salary']:
 del df['salary']
 
 df.to_csv("processed.csv")
+
+def split(df):
+    train, test = train_test_split(df, test_size=.2, random_state=42)
+    train.to_csv("train_80.csv")
+    test.to_csv("test_20.csv")
---
+++ decision_trees.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 12 15:21:29 2017
+
+@author: Yiwei Xia
+"""
+
+from sklearn import tree
+
