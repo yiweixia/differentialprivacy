9d55cd4c3fc771d518b9246823b3d6b77fdc7356
yiwei x <yiwei.x3@gmail.com>
2017-04-12 15:13:11 -0400

    added files

---
+++ cleaner.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 12 15:02:23 2017
+
+@author: Yiwei Xia
+"""
+
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv("raw.csv")
+
+for column in ['sales', 'salary']:
+    dummies = pd.get_dummies(df[column])
+    df[dummies.columns] = dummies
+
+del df['sales']
+del df['salary']
+
+df.to_csv("processed.csv")
